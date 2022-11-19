de6a734f42090e5f20c74b321c4872a3bf643e79
NayeliSeg <ns9888785@gmail.com>
2022-11-19 16:27:23 -0600

    agregar archivo php

---
+++ sumaNumeros.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>
+            Suma de números 
+        </title>
+    </head>
+
+    <body>
+        <form action="" method = "post">
+        <p>A: <input type = "number" name="NA"/></p>
+
+        <p>B: <input type = "number" name="NB"/></p>
+
+        <p><input type="submit" value="Suma"/> </p> 
+        </form>
+
+        <?php
+        $A = $_POST['NA'] ?? null;
+
+        $B = $_POST['NB'] ?? null;
+        
+        $C = ($A + $B);
+
+        echo "C: " . $C;
+        ?>
+    </body>
+</html>
