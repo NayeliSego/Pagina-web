243901d33ef45c0f946faf0cfae28ae8d6155fd6
NayeliSeg <ns9888785@gmail.com>
2022-11-19 12:47:31 -0600

    se agregaron dos nuevos documentos "php, y html"

---
+++ formu.php
@@ -0,0 +1,14 @@
+<?php
+        $A = $_POST['a'] ?? null;
+
+        $B = $_POST['b'] ?? null;
+
+        if ($A=18 & $B=pw)
+        
+        if ($A > $B) {
+              echo "<h2>los intereses personales son:</h2>";
+        echo "intereses: " . $nombre . "<br/>";
+        }
+
+       
+        ?>
